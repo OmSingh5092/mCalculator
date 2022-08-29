a3ebec3db8208ac9f8fca1818f5ad1e0fa33c2db
OmSingh5092 <omsingh6903@gmail.com>
2022-08-29 12:10:36 +0530

    First commit

---
+++ src/main/java/com/example/mCalculator/MCalculatorApplication.java
@@ -0,0 +1,13 @@
+package com.example.mCalculator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MCalculatorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MCalculatorApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/mCalculator/MCalculatorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.mCalculator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MCalculatorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
